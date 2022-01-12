945820b7e43ecc02a9177f0579075d87cfe3ccad
Luís Paulo da Silva Ferreira <luisferreira@souunilavras.com>
2022-01-11 22:33:54 -0300

    Ajuste migrations

--- database/migrations/2022_01_10_033599_create_candidate_quotes.php
+++ database/migrations/2022_01_10_033599_create_candidate_quotes.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateCandidateQuotesTable extends Migration
+class CreateCandidateQuotes extends Migration
 {
     /**
      * Run the migrations.
@@ -19,11 +19,21 @@ class CreateCandidateQuotesTable extends Migration
             $table->bigInteger('user_id')->unsigned()->nullable();
             $table->foreign('user_id')->references('id')->on('users');
 
+            $table->bigInteger('company_id')->unsigned()->nullable(); // caso use id da empresa no lugar do usuario
+            $table->foreign('company_id')->references('id')->on('companies');
+
             $table->bigInteger('quote_id')->unsigned()->nullable();
             $table->foreign('quote_id')->references('id')->on('quotes');
 
-            $table->decimal('price', 4, 2)->nullable();
-            $table->text('comment')->nullable();
+            $table->decimal('price', 4, 2);
+            $table->text('comment');
+
+            $table->dateTime('deadline');
+            $table->boolean('taxes')->default(true); //detalhes
+            $table->boolean('expenditure')->default(true); //detalhes
+
+            $table->string('path_file')->nullable();
+            $table->string('accepted_proposal')->nullable();
 
             // $table->bigInteger('city_id')->unsigned()->nullable();
             // $table->foreign('city_id')->references('id')->on('cities');
