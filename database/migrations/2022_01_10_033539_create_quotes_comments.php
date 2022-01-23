9b55549c682ca2a21d1722ae8e6f19246623c465
Thiago Silva <thiago.silva@gruposn.com.br>
2022-01-23 17:39:48 -0300

    Cotação - Migration da tabela que salvas interações

---
+++ database/migrations/2022_01_10_033539_create_quotes_comments.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCandidateQuotes extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('quote_comments', function (Blueprint $table) {
+            $table->bigIncrements('id');
+
+            $table->bigInteger('user_id')->unsigned()->nullable();
+            $table->foreign('user_id')->references('id')->on('users');
+
+            $table->bigInteger('quote_id')->unsigned()->nullable();
+            $table->foreign('quote_id')->references('id')->on('quotes');
+
+            $table->bigInteger('candidate_id')->unsigned()->nullable();
+            $table->foreign('candidate_id')->references('id')->on('candidate_quotes');
+
+            $table->text('comment');
+
+            $table->boolean('finish_quote')->default(true);
+
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('quote_comments');
+    }
+}
