fbd82c1708a1abc16b93d1999442686c59e0924d
Thiago Silva <thiago.silva@gruposn.com.br>
2022-02-09 18:56:58 -0300

    Cotação - Tela que exibe os totais do NPS - fnalizada

--- app/Http/Controllers/Admin/NpsController.php
+++ app/Http/Controllers/Admin/NpsController.php
@@ -23,14 +23,19 @@ class NpsController extends Controller
         })
             ->paginate(config('myconfig.paginate'));
         
-            $promotor = count(Nps::where('answer', '>=8')->get() ) ? count( Nps::where('answer', '>=8')->get()) : 0 ;
-            $neutro = count(Nps::where('answer', '>= 6')->where('answer', '<= 7')->get() ) ? count( Nps::where('answer', '>= 6')->where('answer', '<= 7')->get()) : 0;
-            $detrator = count(Nps::where('answer', '< 6')->get() ) ? count( Nps::where('answer', '< 6')->get()) : 0;
+            //Totais
+            $promotor =  Nps::where('answer', '>=', 8)->get();
+            $neutro =  Nps::whereIn('answer',[6,7])->get();
+            $detrator =  Nps::where('answer', '<', 6)->get();
 
-            ############################
-            # FALTA CAUCULAR AS MÉDIAS #
-            ############################
+            $totais = Nps::get();
 
-        return view('admin.nps.index', compact('breadcrumb', 'nps', 'promotor', 'neutro', 'detrator'));
+            //Médias
+            $mediaGeral = count($totais) > 0 ? $totais->sum('answer') / count($totais) : 0;
+            $mediaPromotor = count($promotor) > 0 ? $promotor->sum('answer') / count($promotor) : 0;
+            $mediaNeutro = count($neutro) > 0 ? $neutro->sum('answer') / count($neutro) : 0;
+            $mediaDetrator = count($detrator) > 0 ? $detrator->sum('answer') / count($detrator) : 0;
+
+        return view('admin.nps.index', compact('breadcrumb', 'nps', 'promotor', 'neutro', 'detrator', 'mediaGeral', 'mediaPromotor', 'mediaNeutro', 'mediaDetrator' ));
     }
 }
--- resources/views/admin/nps/index.blade.php
+++ resources/views/admin/nps/index.blade.php
@@ -6,7 +6,7 @@
 <!-- Cards com os totais -->
 <div class="row">
     <!-- Card com os totais -->
-    <div class="col-lg-4 col-xs-6">
+    <div class="col-lg-3 col-xs-6">
         <div class="small-box bg-aqua">
             <div class="inner">
                 <h3>{{count($nps)}}</h3>
@@ -15,43 +15,47 @@
             <div class="icon">
                 <i class="ion ion-pie-graph"></i>
             </div>
+            <span class="small-box-footer"> Média Geral: <strong>{{number_format($mediaGeral, 2)}}</strong></span>
         </div>
     </div>
     <!-- Card com os promotores -->
-    <div class="col-lg-4 col-xs-6">
+    <div class="col-lg-3 col-xs-6">
         <!-- small box -->
         <div class="small-box bg-green">
             <div class="inner">
-                <h3>{{$promotor}}</h3>
+                <h3>{{count($promotor)}}</h3>
                 <p>Promotores</p>
             </div>
             <div class="icon">
                 <i class="ion ion-stats-bars"></i>
             </div>
+            <span class="small-box-footer"> Média: <strong>{{number_format($mediaPromotor, 2)}}</strong></span>
         </div>
     </div>
     <!-- Card com os neutros -->
-    <div class="col-lg-4 col-xs-6">
+    <div class="col-lg-3 col-xs-6">
         <div class="small-box bg-yellow" title="">
             <div class="inner">
-            <h3>{{$neutro}}</h3>
+            <h3>{{count($neutro)}}</h3>
                 <p>Neutros</p>
             </div>
             <div class="icon">
                 <i class="ion ion-pie-graph"></i>
             </div>
+            <span class="small-box-footer"> Média: <strong>{{number_format($mediaNeutro, 2)}}</strong></span>
         </div>
     </div>
     <!-- Card com os detratores -->
-    <div class="col-lg-4 col-xs-6">
+    <div class="col-lg-3 col-xs-6">
         <div class="small-box bg-red" title="">
             <div class="inner">
-            <h3>{{$detrator}}</h3>
+            <h3>{{count($detrator)}}</h3>
                 <p>Detratores</p>
             </div>
             <div class="icon">
                 <i class="ion ion-pie-graph"></i>
             </div>
+            <span class="small-box-footer"> Média: <strong>{{number_format($mediaDetrator, 2)}}</strong></span>
         </div>
     </div>
     
@@ -82,8 +86,8 @@
                                             <td>
                                                 @if($item->answer >= 8)
                                                     <span class="btn-sm btn-success">{{$item->answer}}</span>
-                                                @elseif($item->answer >= 6 && <= 7)
-                                                    <span class="btn-sm btn-yellow">{{$item->answer}}</span>
+                                                @elseif($item->answer > 5 && $item->answer < 8)
+                                                    <span class="btn-sm btn-warning">{{$item->answer}}</span>
                                                 @else
                                                     <span class="btn-sm btn-danger">{{$item->answer}}</span>
                                                 @endif
